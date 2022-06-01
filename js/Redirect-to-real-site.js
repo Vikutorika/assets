8c00f0ffa80bddda8c1d5828a9eeceafc8366b66
GamerNoTitle <GamerNoTitle@outlook.com>
2022-06-01 18:23:10 +0800

    Added Some Pictures for my Hexo Blog

--- js/Redirect-to-real-site.js
+++ js/Redirect-to-real-site.js
@@ -4,7 +4,7 @@ if (domain != 'localhost' & domain != 'bili33.top' & domain != '127.0.0.1'){ // 
     window.location=('http://bili33.top'+pathname);
 }
 else {
-    console.log('[GamerNoTitle] 你正在访问GamerNoTitle的小站，请注意，某些站点直接使用了本站在Github用作备份的源码进行部署，并且直称其为**原创作者**，请勿相信！请认真本站点https://bili33.top！')
+    console.log('[GamerNoTitle] 你正在访问GamerNoTitle的小站，请注意，某些站点直接使用了本站在Github用作备份的源码进行部署，并且直称其为**原创作者**，请勿相信！请认准本站点https://bili33.top')
 }
 
 // Fucking fake website that use my source code to deploy yours
