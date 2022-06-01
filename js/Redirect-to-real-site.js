256e03484b72b70006d29a39073613d14b25e3c8
GamerNoTitle <GamerNoTitle@outlook.com>
2022-06-01 18:15:48 +0800

    Added Some Pictures for my Hexo Blog

--- js/Redirect-to-real-site.js
+++ js/Redirect-to-real-site.js
@@ -3,3 +3,5 @@ if (domain != 'localhost' | domain != 'bili33.top'| domain != '127.0.0.1'){ // l
     var pathname = window.location.pathname;
     window.location=('http://bili33.top'+pathname);
 }
+// Fucking fake website that use my source code to deploy yours
+// 去你的直接拿我的网站源码去部署你们所谓的**自己的站点**
