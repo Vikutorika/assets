63b5fbfbbbac6f72b688b567548aba9b8b9b940c
GamerNoTitle <GamerNoTitle@outlook.com>
2022-06-01 18:13:22 +0800

    Added Some Pictures for my Hexo Blog

--- js/Redirect-to-real-site.js
+++ js/Redirect-to-real-site.js
@@ -1,2 +1,5 @@
 var domain = document.domain;
-console.log(domain)
+if (domain != 'localhost' | domain != 'bili33.top'| domain != '127.0.0.1'){
+    var pathname = window.location.pathname;
+    window.location=('http://bili33.top'+pathname);
+}
