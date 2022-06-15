aece62516be8512951f71a8cb81de5bf49825e34
GamerNoTitle <GamerNoTitle@outlook.com>
2022-06-15 22:06:09 +0800

    Added Some Pictures for my Hexo Blog

--- js/CTF-illegal-warning.js
+++ js/CTF-illegal-warning.js
@@ -1,5 +1,5 @@
 var options =  {
-    content: "所有未经允许的入侵均违法！！！（点击本通知以隐藏）", // text of the snackbar
+    content: "你正在浏览CTF相关内容，请注意：所有未经允许的入侵均违法！！！（点击本通知以隐藏）", // text of the snackbar
     style: "toast", // add a custom class to your snackbar
     timeout: 0, // time in milliseconds after the snackbar autohides, 0 is disabled
     htmlAllowed: true, // allows HTML as content value
@@ -9,7 +9,7 @@ var options =  {
 var $ = document.getElementById("snackbar");
 
 if (window.location.pathname.includes('CTF')){
-    alert('你正在浏览CTF相关内容，请注意：未经允许的入侵均违法！！！')
+    //alert('你正在浏览CTF相关内容，请注意：未经允许的入侵均违法！！！')
 
     $.snackbar(options);
 }
