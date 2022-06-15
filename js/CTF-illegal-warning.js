742d917c7d55e5c44a63586690ce32a81a293fc2
GamerNoTitle <GamerNoTitle@outlook.com>
2022-06-15 21:13:33 +0800

    Added Some Pictures for my Hexo Blog

---
+++ js/CTF-illegal-warning.js
@@ -0,0 +1,11 @@
+var options =  {
+    content: "所有未经允许的入侵均违法！！！（点击本通知以隐藏）", // text of the snackbar
+    style: "toast", // add a custom class to your snackbar
+    timeout: 0, // time in milliseconds after the snackbar autohides, 0 is disabled
+    htmlAllowed: true, // allows HTML as content value
+    onClose: function(){ } // callback called when the snackbar gets closed.
+}
+
+if ('CTF' in window.location.pathname){
+$.snackbar(options);
+}
