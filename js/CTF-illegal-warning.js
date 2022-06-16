990f110df22172fac3e76e5e1d0438583eea290f
GamerNoTitle <GamerNoTitle@outlook.com>
2022-06-16 15:26:09 +0800

    Added Some Pictures for my Hexo Blog

--- js/CTF-illegal-warning.js
+++ js/CTF-illegal-warning.js
@@ -1,4 +1,3 @@
-import swal from 'sweetalert';
 if (window.location.pathname.includes("CTF")) {
   // var response = confirm(
   //   "看起来您正在访问CTF相关内容\n请注意：所有未经允许的入侵均违法！！！\n您是否已经理解在进行CTF相关操作时需要遵守相关的法律法规？\n如果您已经理解，请点击确定按钮，否则点击取消返回首页！"
