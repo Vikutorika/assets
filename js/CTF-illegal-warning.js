a630e0504f3dbe965f261f35fe435f2afccc5683
GamerNoTitle <GamerNoTitle@outlook.com>
2022-06-16 16:34:10 +0800

    Added Some Pictures for my Hexo Blog

--- js/CTF-illegal-warning.js
+++ js/CTF-illegal-warning.js
@@ -1,6 +1,6 @@
 if (window.location.pathname.includes("CTF") && localStorage.getItem('CTF-Agreement') == null) {
   var response = confirm(
-    "看起来您正在访问CTF相关内容\n请注意：所有未经允许的入侵均违法！！！\n您是否已经理解在进行CTF相关操作时需要遵守相关的法律法规？\n如果您已经理解，请点击确定按钮，否则点击取消返回首页！"
+    "看起来您正在访问CTF相关内容\n请注意：所有未经允许的入侵均违法！！！\n您是否已经理解在进行CTF相关操作时需要遵守相关的法律法规？\n如果您已经理解，请点击确定按钮，否则点击取消返回首页！\n（本消息只会显示一次，一旦点击确定后将不再显示）"
   );
   if (response) {
     localStorage.setItem('CTF-Agreement', 'true');
