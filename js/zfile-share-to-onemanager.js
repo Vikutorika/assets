fcd1dadaaa3ba225deb38f8eac1a33a3aa835ca5
GamerNoTitle <GamerNoTitle@outlook.com>
2022-07-08 22:37:56 +0800

    Added Some Pictures for my Hexo Blog

--- js/zfile-share-to-onemanager.js
+++ js/zfile-share-to-onemanager.js
@@ -1,6 +1,6 @@
-if(window.location.pathname=="/s/54dwlv"){
+if(window.location.pathname=="/s/54dwlv" || window.location.pathname=="/Default/s/54dwlv"){
     window.location('https://share4nothing.cf/Public/%E9%9F%B3%E7%81%B5%E5%85%A8%E8%A7%A3%E9%94%81%E5%AD%98%E6%A1%A3.reg')
 }
-else if(window.location.pathname == "/s/sdf0f6"){
+else if(window.location.pathname == "/s/sdf0f6" || window.location.pathname=="/Default/s/sdf0f6"){
     window.location('https://share4nothing.cf/Public/INVAXION%20100%20Percent%20Save.reg')
 }
