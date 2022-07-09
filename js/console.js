fec930d9f8ff32e6a8b31425b441ab345a04873c
GamerNoTitle <GamerNoTitle@outlook.com>
2022-07-09 10:14:26 +0800

    Added Some Pictures for my Hexo Blog

--- js/console.js
+++ js/console.js
@@ -14,17 +14,19 @@ if (domain == "bili33.top" || domain == "localhost" || domain == "127.0.0.1") {
     " / /_//  __/ |_) | |_| | (_| | (_| |  __/ |   \n" +
     "/___,' \\___|_.__/ \\__,_|\\__, |\\__, |\\___|_|   \n" +
     "   --GamerNoTitle       |___/ |___/           \n";
-}
-else if (domain == 'share4nothing.cf' || domain == 'share4nothing.ml' || domain == 'sassy-angry-sale.glitch.me'){
-    log = 
+} else if (
+  domain == "share4nothing.cf" ||
+  domain == "share4nothing.ml" ||
+  domain == "sassy-angry-sale.glitch.me"
+) {
+  log =
     " __ _                      _                                     _   _           _     _ _   \n" +
     "/ _\\ |__   __ _ _ __ ___  (_)___    __ _    __ _  ___   ___   __| | | |__   __ _| |__ (_) |_ \n" +
     "\\ \\| '_ \\ / _` | '__/ _ \\ | / __|  / _` |  / _` |/ _ \\ / _ \\ / _` | | '_ \\ / _` | '_ \\| | __|\n" +
     "_\\ \\ | | | (_| | | |  __/ | \\__ \\ | (_| | | (_| | (_) | (_) | (_| | | | | | (_| | |_) | | |_ \n" +
     "\\__/_| |_|\\__,_|_|  \\___| |_|___/  \\__,_|  \\__, |\\___/ \\___/ \\__,_| |_| |_|\\__,_|_.__/|_|\\__|\n" +
-"                                           |___/                 --GamerNoTitle              \n"
-}
-else{
-    log=undefined
+    "                                           |___/                 --GamerNoTitle              \n";
+} else {
+  log = undefined;
 }
 console.log(log);
