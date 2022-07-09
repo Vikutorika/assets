fc514a05ed68d34ff990e9be03a41f930c7c387e
GamerNoTitle <GamerNoTitle@outlook.com>
2022-07-09 09:46:42 +0800

    Added Some Pictures for my Hexo Blog

--- js/Redirect-to-real-site.js
+++ js/Redirect-to-real-site.js
@@ -1,12 +1,26 @@
 var domain = document.domain;
-if (domain != 'localhost' & domain != 'bili33.top' & domain != '127.0.0.1'){ // localhost & 127.0.0.1 for local debugging, bili33.top is my domain
-    alert('看起来您正在访问的网页 ' + window.location.href + ' 并非官方网页（即该站站长可能直接使用了我的源码并部署了他自己的网站），请您谅解，我们将为您跳转到原网站。')
+if (
+  (domain != "localhost") &
+  (domain != "bili33.top") &
+  (domain != "127.0.0.1")
+) {
+  // localhost & 127.0.0.1 for local debugging, bili33.top is my domain
+  if (domain == "webcache.googleusercontent.com") {
+    usecached = confirm(
+      "It seems that you are accessing a cached page. Do you want to redirect to the real-time site?"
+    );
+    if (usecached) {
+      window.location.href = "http://bili33.top/";
+    }
+  } else {
+    alert(
+      "看起来您正在访问的网页 " +
+        window.location.href +
+        " 并非官方网页，有可能您正在访问Demo站，我们将为您跳转到原网站。"
+    );
     var pathname = window.location.pathname;
-    window.location=('http://bili33.top'+pathname);
+    window.location = "http://bili33.top" + pathname;
+  }
 }
-else {
-    console.log('[GamerNoTitle] 你正在访问GamerNoTitle的小站，请注意，某些站点直接使用了本站在Github用作备份的源码进行部署，并且直称其为**原创作者**，请勿相信！请认准本站点https://bili33.top')
-}
-
 // Fucking fake website that use my source code to deploy yours
 // 去你的直接拿我的网站源码去部署你们所谓的**自己的站点**
