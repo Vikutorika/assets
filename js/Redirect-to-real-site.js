b99faaab91a3ef90e5dc549ce7f777957726522e
GamerNoTitle <GamerNoTitle@outlook.com>
2022-07-22 10:52:47 +0800

    Added Some Pictures for my Hexo Blog

--- js/Redirect-to-real-site.js
+++ js/Redirect-to-real-site.js
@@ -12,6 +12,14 @@ if (
     if (usecached) {
       window.location.href = "http://bili33.top/";
     }
+  } else if (
+    domain == "blog.ninym.top" &&
+    localStorage.getItem("Mirror-Agreement") == null
+  ) {
+    usemirror = confirm(
+      "你正在访问镜像站，只有主站不可用的时候我们才推荐你访问镜像站\n按确定继续访问，或者按取消返回主站\n（确定后此提示不再弹出）"
+    );
+    localStorage.setItem("Mirror-Agreement", "true");
   } else {
     alert(
       "看起来您正在访问的网页 " +
