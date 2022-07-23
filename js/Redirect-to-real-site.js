e14181b2864440682463dd59bae0961978c68dab
GamerNoTitle <GamerNoTitle@outlook.com>
2022-07-23 14:25:18 +0800

    Added Some Pictures for my Hexo Blog

--- js/Redirect-to-real-site.js
+++ js/Redirect-to-real-site.js
@@ -13,15 +13,9 @@ if (
     if (usecached) {
       window.location.href = "http://bili33.top/";
     }
-  } else {
-    alert(
-      "看起来您正在访问的网页 " +
-        window.location.href +
-        " 并非官方网页，有可能您正在访问Demo站，我们将为您跳转到原网站。"
-    );
-    var pathname = window.location.pathname;
-    window.location = "http://bili33.top" + pathname;
-  }
+    else {
+      undefined
+    }
 } else if (
   domain == "blog.ninym.top" &&
   localStorage.getItem("Mirror-Agreement") == null
@@ -30,6 +24,15 @@ if (
     "你正在访问镜像站，只有主站不可用的时候我们才推荐你访问镜像站\n按确定继续访问，或者按取消返回主站\n（确定后此提示不再弹出）"
   );
   localStorage.setItem("Mirror-Agreement", "true");
+}
+} else {
+  alert(
+    "看起来您正在访问的网页 " +
+      window.location.href +
+      " 并非官方网页，有可能您正在访问Demo站，我们将为您跳转到原网站。"
+  );
+  var pathname = window.location.pathname;
+  window.location = "http://bili33.top" + pathname;
 }
 // Fucking fake website that use my source code to deploy yours
 // 去你的直接拿我的网站源码去部署你们所谓的**自己的站点**
