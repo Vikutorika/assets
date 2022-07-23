86c362d9c9b85340f503e2e099dcc4fcd83dd51d
GamerNoTitle <GamerNoTitle@outlook.com>
2022-07-23 09:08:58 +0800

    Added Some Pictures for my Hexo Blog

--- js/Redirect-to-real-site.js
+++ js/Redirect-to-real-site.js
@@ -2,7 +2,8 @@ var domain = document.domain;
 if (
   (domain != "localhost") &
   (domain != "bili33.top") &
-  (domain != "127.0.0.1")
+  (domain != "127.0.0.1") &
+  (domain != "blog.ninym.top")
 ) {
   // localhost & 127.0.0.1 for local debugging, bili33.top is my domain
   if (domain == "webcache.googleusercontent.com") {
@@ -12,14 +13,6 @@ if (
     if (usecached) {
       window.location.href = "http://bili33.top/";
     }
-  } else if (
-    domain == "blog.ninym.top" &&
-    localStorage.getItem("Mirror-Agreement") == null
-  ) {
-    usemirror = confirm(
-      "你正在访问镜像站，只有主站不可用的时候我们才推荐你访问镜像站\n按确定继续访问，或者按取消返回主站\n（确定后此提示不再弹出）"
-    );
-    localStorage.setItem("Mirror-Agreement", "true");
   } else {
     alert(
       "看起来您正在访问的网页 " +
@@ -29,6 +22,14 @@ if (
     var pathname = window.location.pathname;
     window.location = "http://bili33.top" + pathname;
   }
+} else if (
+  domain == "blog.ninym.top" &&
+  localStorage.getItem("Mirror-Agreement") == null
+) {
+  usemirror = confirm(
+    "你正在访问镜像站，只有主站不可用的时候我们才推荐你访问镜像站\n按确定继续访问，或者按取消返回主站\n（确定后此提示不再弹出）"
+  );
+  localStorage.setItem("Mirror-Agreement", "true");
 }
 // Fucking fake website that use my source code to deploy yours
 // 去你的直接拿我的网站源码去部署你们所谓的**自己的站点**
