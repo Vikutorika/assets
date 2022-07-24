0b15f52e67800544c0dc1b1b20cfec13c793e071
GamerNoTitle <GamerNoTitle@outlook.com>
2022-07-24 08:01:43 +0800

    Added Some Pictures for my Hexo Blog

--- js/Redirect-to-real-site.js
+++ js/Redirect-to-real-site.js
@@ -1,12 +1,24 @@
+// localhost & 127.0.0.1 for local debugging, bili33.top is my domain
+// Fucking fake website that use my source code to deploy yours
+// 去你的直接拿我的网站源码去部署你们所谓的**自己的站点**
 var domain = document.domain;
-if (
-  (domain != "localhost") &
-  (domain != "bili33.top") &
-  (domain != "127.0.0.1") &
-  (domain != "blog.ninym.top")
-) {
-  // localhost & 127.0.0.1 for local debugging, bili33.top is my domain
-  if (domain == "webcache.googleusercontent.com") {
+switch (domain) {
+  case "bili33.top":
+    undefined;
+  case "127.0.0.1":
+    undefined;
+  case "localhost":
+    undefined;
+  case "blog.ninym.top":
+    if (localStorage.getItem("Mirror-Agreement") == null) {
+      usemirror = confirm(
+        "你正在访问镜像站，只有主站不可用的时候我们才推荐你访问镜像站\n按确定继续访问，或者按取消返回主站\n（确定后此提示不再弹出）"
+      );
+      if (usemirror) {
+        localStorage.setItem("Mirror-Agreement", "true");
+      }
+    }
+  case "webcache.googleusercontent.com":
     usecached = confirm(
       "It seems that you are accessing a cached page. Do you want to redirect to the real-time site?"
     );
@@ -15,23 +27,12 @@ if (
     } else {
       undefined;
     }
-  }
-} else if (
-  domain == "blog.ninym.top" &&
-  localStorage.getItem("Mirror-Agreement") == null
-) {
-  usemirror = confirm(
-    "你正在访问镜像站，只有主站不可用的时候我们才推荐你访问镜像站\n按确定继续访问，或者按取消返回主站\n（确定后此提示不再弹出）"
-  );
-  localStorage.setItem("Mirror-Agreement", "true");
-} else {
-  alert(
-    "看起来您正在访问的网页 " +
-      window.location.href +
-      " 并非官方网页，有可能您正在访问Demo站，我们将为您跳转到原网站。"
-  );
-  var pathname = window.location.pathname;
-  window.location = "http://bili33.top" + pathname;
+  default:
+    alert(
+      "看起来您正在访问的网页 " +
+        window.location.href +
+        " 并非官方网页，有可能您正在访问Demo站，我们将为您跳转到原网站。"
+    );
+    var pathname = window.location.pathname;
+    window.location = "http://bili33.top" + pathname;
 }
-// Fucking fake website that use my source code to deploy yours
-// 去你的直接拿我的网站源码去部署你们所谓的**自己的站点**
